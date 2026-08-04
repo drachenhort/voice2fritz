8df2e32fee9caed23248adbae24ea99c86a6394b
Stephan <stephan.schiemann@gmail.com>
2026-08-04 19:43:09 +0200

    style: halve vertical padding/min-height on dialpad buttons

--- src/voice2fritz/gui/main_window.py
+++ src/voice2fritz/gui/main_window.py
@@ -66,7 +66,7 @@ class MainWindow(QMainWindow):
             for col, digit in enumerate(digits):
                 button = QPushButton(digit)
                 button.setObjectName("dialpadButton")
-                button.setMinimumSize(80, 36)
+                button.setMinimumSize(80, 18)
                 button.clicked.connect(lambda checked=False, d=digit: self._on_digit_clicked(d))
 
                 letters_label = QLabel(_T9_LETTERS[digit])
